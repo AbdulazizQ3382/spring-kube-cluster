c0a26c2788ce8b65d665640e600651f5aaf1e24f
abdulazizQ3382 <abuqannam2@gmail.com>
2023-09-25 10:29:41 +0300

    spring boot and postgres installed in kube template

---
+++ src/main/java/com/example/springkubecluster/SpringKubeClusterApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkubecluster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKubeClusterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKubeClusterApplication.class, args);
+    }
+
+}
